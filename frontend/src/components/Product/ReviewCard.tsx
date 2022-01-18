e276a42666dec220f1008d62d9108bcb984a9a3b
IsmaelDev13 <ismael-diaz8@hotmail.es>
2022-01-18 11:20:13 +0100

    user bub

--- frontend/src/components/Product/ReviewCard.tsx
+++ frontend/src/components/Product/ReviewCard.tsx
@@ -16,7 +16,7 @@ export const ReviewCard: React.FC<ReviewCardProps> = ({ review }) => {
   };
   return (
     <div className="flex flex-col items-center p-5 border-t-2  shadow-xl rounded-md">
-      <img className="h-10 w-10 rounded-full" src={user.avatar.url} alt="" />
+      <img className="h-10 w-10 rounded-full" src={user?.avatar.url} alt="" />
       <p>{review.name}</p>
       <Rating {...options} />
       <span>{review.comment}</span>
